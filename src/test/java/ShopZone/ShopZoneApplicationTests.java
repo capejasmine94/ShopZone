254015f26934cfb8887ed0bd5909235d45a588fe
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-11-28 16:58:06 +0900

    Initial commit

---
+++ src/test/java/ShopZone/ShopZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ShopZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
