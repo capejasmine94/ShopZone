771b9614be5881cc09102fd4b32d877cb8f4fbbd
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-11-28 19:59:35 +0900

    fix: 관리자 로그인,회원가입 경로 수정

--- src/main/java/ShopZone/admin/controller/AdminController.java
+++ src/main/java/ShopZone/admin/controller/AdminController.java
@@ -2,18 +2,25 @@ package ShopZone.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("admin")
 public class AdminController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String adminLogin() {
         return "admin/adminLogin";
     }
 
-    @GetMapping("/adminRegister")
+    @GetMapping("adminRegister")
     public String adminRegister() {
         return "admin/adminRegister";
+    }
+
+    @GetMapping("adminRegisterComplete")
+    public String adminRegisterComplete() {
+        return "admin/adminRegisterComplete";
     }
 
 }
